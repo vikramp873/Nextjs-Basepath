b08d00b3b13e06dbf8319cd471118c5bf7e7d395
vikram parmar <vikram.parmar@crimsoni.com>
2024-02-27 11:33:28 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/Researcher-Education',
+
+    async redirects() {
+        return [
+          {
+            source: '/testone',
+            destination: '/testtwo',
+            basePath: false,
+            permanent:false
+          },
+        ]
+      },
+
+};
 
 export default nextConfig;
---
+++ src/app/testone/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>test one</div>
+  )
+}
+
+export default page
